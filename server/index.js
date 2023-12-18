480a31b70a5185ef5c03f3662aa6ac13960a77ba
pournimaanjole <pournimaanjole@gmail.com>
2023-12-18 21:55:45 +0530

    host on netlify

--- server/index.js
+++ server/index.js
@@ -2,11 +2,13 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 dotenv.config();
+import path from 'path';
 
 const app = express();
 app.use(express.json())
 const PORT = process.env.PORT || 5000
 
+const __diname =path.resolve();
 
 import getHealthApi from './controlers/health.js';
 import { postTransactionApi,gettransactionsApi } from './controlers/transaction.js';
@@ -38,6 +40,14 @@ app.post('/api/v1/signups' ,userSignup)
 // login post request 
 
 app.post('/api/v1/login' , userLogin)
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'))
+    })
+}
 
 
 app.listen(PORT , ()=>{
